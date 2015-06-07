57c9f62876c0a0ce6e36cb94b022ede99d06bf65
daedric123 <emiliakey@NSA-Mon-X1023-4.hsd1.ca.comcast.net>
2015-06-07 15:34:56 -0700

    fixed and cleaned up with more notes
    Basically thats it.

--- SPARKIPROJECT2.0/SPARKIPROJECT2.0.ino
+++ SPARKIPROJECT2.0/SPARKIPROJECT2.0.ino
@@ -1,11 +1,10 @@
 #include <Sparki.h>  // include the sparki library
 // We should do this in SETUP .. not Loop
-// Making new notes
+// Loop is for continously repeating code. Setup is linear and will run only once
 
 
 void setup()
 {
-
     sparki.beep();       // Sparki beeps!
     delay(1000);         // wait a second (1000 milliseconds)
     
@@ -15,12 +14,11 @@ void setup()
     sparki.beep();
     delay(1000);
 
+    sparki.moveForward();
+    delay(10000);
 
-	sparki.moveForward();
-	delay(10000);
-
-	sparki.moveStop();
-	delay(1000);
+    sparki.moveStop();
+    delay(1000);
 
 }
 void loop()
