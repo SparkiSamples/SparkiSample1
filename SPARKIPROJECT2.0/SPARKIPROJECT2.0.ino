7691311accd01fdd038a314683f7f3fafc581da2
daedric123 <emiliakey@NSA-Mon-X1023-4.hsd1.ca.comcast.net>
2015-06-07 15:18:24 -0700

    1st check in

---
+++ SPARKIPROJECT2.0/SPARKIPROJECT2.0.ino
@@ -0,0 +1,30 @@
+#include <Sparki.h>  // include the sparki library
+
+void setup()
+{
+}
+void loop()
+{
+    sparki.beep();       // Sparki beeps!
+    delay(1000);         // wait a second (1000 milliseconds)
+    
+     sparki.beep();
+     delay(1000);
+
+    sparki.beep();
+    delay(1000);
+
+
+sparki.moveForward();
+delay(10000);
+
+sparki.moveStop();
+delay(1000);
+
+    
+}
+
+
+
+
+  
